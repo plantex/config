d9d72708a989287215bd44479d48f73e2e17a148
Mathieu Plantey <mathieu.plantey@blade-group.com>
2019-11-22 16:58:13 +0100

    adding deployment script

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+TMP=`dirname $0`
+GIT_REPO=`readlink -m $TMP`
+GIT_HOME=$GIT_REPO/home
+
+symlink() {
+    if [ $# -ne 1 ]
+    then
+        exit
+    fi
+    local PATH_FROM_HOME=$1
+    local CURRENT_DIR=`readlink -m $GIT_HOME/$PATH_FROM_HOME`
+    local MY_HOME=`readlink -m $HOME/$PATH_FROM_HOME`
+    for file in `ls -A $CURRENT_DIR`
+    do
+        if [ -d $CURRENT_DIR/$file ]
+        then
+            symlink $PATH_FROM_HOME/$file
+        else
+            if [ -f $MY_HOME/$file ] && [ ! -h $MY_HOME/$file ]
+            then
+                mv $MY_HOME/$file $MY_HOME/$file.bak
+            fi
+            ln -sf $CURRENT_DIR/$file $MY_HOME/$file
+            ls -lh $MY_HOME/$file
+        fi
+    done
+}
+
+symlink .
